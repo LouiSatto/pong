2e8740e8f62b4cfb038961c066756ad77f4bc8f3
Loui Satto <x_sato@hotmail.com>
2020-08-27 18:26:02 +0900

    paddle moving

--- main.lua
+++ main.lua
@@ -1,14 +1,79 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+push = require 'push'
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallfont = love.graphics.newFont('font.ttf', 8)
+    scorefont = love.graphics.newFont('font.ttf', 32)
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 20
+    player2Y = VIRTUAL_HEIGHT - 40
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
         resizable = false
     })
 end
 
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+
+        player1Y = player1Y - PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('s') then
+        
+        player1Y = player1Y + PADDLE_SPEED * dt
+
+    end
+
+    if love.keyboard.isDown('up') then
+
+        player2Y = player2Y - PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('down') then
+
+        player2Y = player2Y + PADDLE_SPEED * dt
+
+    end
+
+
+end
+
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
-    love.graphics.printf("Hello Pong", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, "center" )
+    push:apply('start')
+    
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+    
+    love.graphics.rectangle('fill', 5, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    
+    
+    love.graphics.setFont(smallfont)
+    love.graphics.printf("Hello Pong", 0, 20, VIRTUAL_WIDTH, "center" )
+    
+    love.graphics.setFont(scorefont)
+    love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 -50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 +30, VIRTUAL_HEIGHT / 3)
+
+    push:apply('end')
 end
